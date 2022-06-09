2f03b022381a51773a91f406ee1808d3b50d3a69
Cássia ☆ <102737875+cassiaplima@users.noreply.github.com>
2022-06-09 11:01:59 -0300

    Add files via upload

---
+++ atividades.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class atividades {
+    public static void main(String[] args){
+        Scanner inteiros = new Scanner(System.in);
+        /* 
+        // atividade 1
+        System.out.println("Me de dois numero inteiro");
+        int alunos = inteiros.nextInt();
+        int alunos0 = inteiros.nextInt();
+        int soma = alunos + alunos0;
+        System.out.println("o seu soma deu :" + soma);
+*/
+        /* 
+         // atividade 2
+        System.out.println("digite o seu metros");
+        int metros = inteiros.nextInt();
+        int centimetos = metros*100;
+        System.out.println("a sua responta foi essa: " + centimetos);
+       */
+
+       /* 
+       //atividade 3 
+       System.out.println("digite o seu numero");
+       int inpar = inteiros.nextInt();
+    
+       if(inpar % 2 == 0){
+           System.out.println("O seu deu par");
+       }else{
+           System.out.println("O seu deu impar");
+       }
+       */
+      /* 
+       // atividade 4 
+       System.out.println("digite a sua velocidade");
+       int velocidade = inteiros.nextInt();
+
+       if(velocidade > 80){
+           int multa = (velocidade - 80)/5;
+           System.out.println("parabens! vc foi multado" + multa);
+       }else{
+           System.out.println("voce esta dentro do limite");
+       }
+    */
+    }
+}
